e0dd705b76c60edd3e6a23c6165084ee8ff1f194
maxinegrete1010-svg <maxinegrete1010@gmail.com>
2026-07-11 05:02:15 -0300

    Agrega Firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBw11PvjkEp4MqrKEJVikCS7r8sgq2iH58",
+  authDomain: "maxruleta-19667.firebaseapp.com",
+  projectId: "maxruleta-19667",
+  storageBucket: "maxruleta-19667.firebasestorage.app",
+  messagingSenderId: "798150344122",
+  appId: "1:798150344122:web:66199a7b262309c9e0b183"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+console.log("Firebase conectado correctamente ✅");
